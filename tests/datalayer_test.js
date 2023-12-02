50e8a7b319f907a3424c5e85288863461a21dcde
Martin Hoffmeister <martin.hoffmeister@verivox.com>
2023-12-02 11:44:15 +0100

    window.datalayer

--- tests/datalayer_test.js
+++ tests/datalayer_test.js
@@ -1,12 +1,16 @@
 Scenario('Access dataLayer using executeScript', async ({ I }) => {
-    I.amOnPage('https://example.com');
+    I.amOnPage('https://example.com'); // Replace with the actual URL of your page
 
-    // Use I.executeScript to execute script in browser context
+    // Use I.executeScript to execute script in the browser context
     const dataLayer = await I.executeScript(() => {
         // The code inside here is executed in the browser context
-        return dataLayer; // Assuming dataLayer is a global object in the browser
+        return window.dataLayer; // Assuming dataLayer is a global object in the browser
     });
 
     // You can now work with the dataLayer in Node.js context
     console.log('dataLayer:', dataLayer);
+
+    // Your assertions or further actions based on the dataLayer content
+    // I.see('ExpectedValue', dataLayer.someProperty); // Replace 'someProperty' and 'ExpectedValue' with your actual dataLayer properties and values
+
 });
