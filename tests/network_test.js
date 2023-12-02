949b23b5282ab67de9e6e263f2207359b5229147
Martin Hoffmeister <martin.hoffmeister@verivox.com>
2023-12-02 11:11:53 +0100

    try network

--- tests/network_test.js
+++ tests/network_test.js
@@ -1,7 +1,25 @@
-Feature('Network traffic')
+// Feature('Network traffic')
 
-Scenario('Verify network traffic @network-traffic', async ({ I }) => {
-    I.amOnPage('/');
-    networkTraffic = await I.executeScript("return window.performance.getEntries();")
-    console.log(networkTraffic);
+// Scenario('Verify network traffic @network-traffic', async ({ I }) => {
+//     I.amOnPage('/');
+//     networkTraffic = await I.executeScript("window.performance.getEntries();")
+//     console.log(networkTraffic);
+// });
+
+Feature('Example Feature');
+
+Scenario('Execute Script Example', (I) => {
+    I.amOnPage('https://example.com');
+
+    // Example of using executeScript to change the page title
+    I.executeScript(() => {
+        document.title = 'New Page Title';
+    });
+
+    // You can also use executeScript to return a value
+    const pageTitle = I.executeScript(() => {
+        return document.title;
+    });
+
+    I.say(`The new page title is: ${pageTitle}`);
 });
