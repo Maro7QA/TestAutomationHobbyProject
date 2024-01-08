3924b058276a1b3ef60f957d94ad4bf6bc752314
Maro7QA <martin.hoffmeister@hotmail.com>
2024-01-08 11:59:24 +0100

    network traffic PoC MagentaSport

--- tests/network_test.js
+++ tests/network_test.js
@@ -1,14 +1,8 @@
-// Feature('Network traffic')
-
-// Scenario('Verify network traffic @network-traffic', async ({ I }) => {
-//     I.amOnPage('/');
-//     networkTraffic = await I.executeScript("window.performance.getEntries();")
-//     console.log(networkTraffic);
-// });
+const landingPage = require("../src/pages/landingPage");
 
 Feature('Example Feature');
 
-Scenario('Execute Script Example', async ({ I }) => {
+xScenario('Execute Script Example', async ({ I }) => {
     I.amOnPage('https://example.com');
 
     // Example of using executeScript to change the page title
@@ -30,4 +24,32 @@ Scenario('Execute Script Example', async ({ I }) => {
     });
 
     console.log(networkTraffic);
+});
+
+Scenario('Tracking', async ({ I }) => {
+    I.amOnPage('/');
+    landingPage.selectAcceptAllCookies();
+    // Example of using executeScript to change the page title
+    I.scrollTo('[class="showcase-calendar"]');
+    I.wait(1);
+    I.scrollTo('(//*[@class="lane-event js-metadata-container"])[1]',0,-200);
+    I.wait(1);
+    I.click('(//*[@class="lane-event js-metadata-container"])[1]');
+    I.wait(3);
+
+    // Use page.evaluate to execute script in browser context
+    const networkTraffic = await I.executeScript(() => {
+        // The code inside here is executed in the browser context
+        return window.performance.getEntries();
+    });
+
+    function containsSubstring(networkTraffic, substring) {
+        return networkTraffic.filter(item => item.name.includes(substring));
+      }
+      
+      // Check if the substring exists in the "name" field
+      const result = containsSubstring(networkTraffic, "https://pix.telekom.de/");
+      
+      // Display the result
+      console.log(result);
 });
