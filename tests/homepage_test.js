4c731ffffa9332212d123ee2500716c1d029787e
Maro7QA <martin.hoffmeister@hotmail.com>
2024-01-10 11:21:00 +0100

    try screen maximization

--- tests/homepage_test.js
+++ tests/homepage_test.js
@@ -5,6 +5,8 @@ Feature("homepage");
 
 Scenario("open homepage @homepage", ({ I }) => {
     I.amOnPage("/");
+     // Maximize the browser window
+    I.resizeWindow(1920, 1080); // Set a large resolution to simulate maximizing
     landingPage.selectAcceptAllCookies();
     landingPage.selectBasketball();
     landingPage.showResults();
