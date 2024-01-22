a2a46ca89d256909702473e2a796406066b2b04a
Maro7QA <martin.hoffmeister@hotmail.com>
2024-01-22 10:00:19 +0100

    remove old test

--- tests/network_test.js
+++ tests/network_test.js
@@ -6,30 +6,6 @@ const expect = require("chai").expect;
 
 Feature('Example Feature');
 
-xScenario('Execute Script Example', async ({ I }) => {
-    I.amOnPage('https://example.com');
-
-    // Example of using executeScript to change the page title
-    I.executeScript(() => {
-        document.title = 'New Page Title';
-    });
-
-    // You can also use executeScript to return a value
-    const pageTitle = I.executeScript(() => {
-        return document.title;
-    });
-
-    I.say(`The new page title is: ${pageTitle}`);
-
-    // Use page.evaluate to execute script in browser context
-    const networkTraffic = await I.executeScript(() => {
-        // The code inside here is executed in the browser context
-        return window.performance.getEntries();
-    });
-
-    console.log(networkTraffic);
-});
-
 let runs = new DataTable(["run"]);
 
 const numbRuns = 1
