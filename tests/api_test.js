05df088fc2811eb812774b83e7ea1c3fe34577e7
Maro7QA <martin.hoffmeister@hotmail.com>
2024-01-22 09:07:39 +0100

    more api tests

--- tests/api_test.js
+++ tests/api_test.js
@@ -1,22 +1,33 @@
 const expect = require("chai").expect;
 
 Feature('API calls')
-Scenario('Verify some basic api-calls Get call @api-calls', async ({ I }) => {
+Scenario('home @api-calls', async ({ I }) => {
     const res = await I.sendGetRequest(
         "https://www.magentasport.de/home"
     );
     expect(res.status).to.eql(200);
 });
-// Scenario('Verify some basic api-calls Post call @api-calls', async ({ I }) => {
-//     bankPayload = {
-//         "iban": "DE02100500000024290661"
-//     }
-//     const response = await I.sendPostRequest(
-//         "https://service.vxdev.de/bank-account-details/v2/account-details",
-//         bankPayload,
-//         {
-//             "Content-Type": "application/vnd.verivox.iban+json"
-//         }
-//     );
-//     expect(response.status).to.eql(200);
-// });
+
+
+Scenario('Programme @api-calls', async ({ I }) => {
+    const res = await I.sendGetRequest(
+        "https://www.magentasport.de/programm"
+    );
+    expect(res.status).to.eql(200);
+});
+
+Scenario('Standings Euroleague @api-calls', async ({ I }) => {
+    const res = await I.sendGetRequest(
+        "https://www.magentasport.de/euroleague/tabelle"
+    );
+    expect(res.status).to.eql(200);
+});
+
+Scenario('Search Euroleague @api-calls', async ({ I }) => {
+    const res = await I.sendGetRequest(
+        "https://www.magentasport.de/home?searchText=Zalgiris%20Kaunas&category=all&page=1"
+    );
+    I.wait(2);
+    expect(res.status).to.eql(200);
+});
+
